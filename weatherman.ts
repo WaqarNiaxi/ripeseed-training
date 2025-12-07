89b15d73c16b0a9662e8f2c7a41b57b5d28c385b
waqar bin zafar <waqar.zafar@ripeseed.io>
2025-12-07 23:26:04 +0500

    revert the code

--- weatherman.ts
+++ weatherman.ts
@@ -1,8 +1,3 @@
-const yearlyReport = require("./src/report/yearlyExtremesReport");
-const monthlyAverageReport = require("./src/report/monthlyAverageReport");
-const monthlyChartReport = require("./src/report/monthlyChartReport");
-const formatDate = require("./src/helper/common");
-import type { ChartReport } from "./src/interfaces/WeatherInterface.types";
 
 
 const args = process.argv.slice(2);
@@ -14,66 +9,25 @@ if (!dataDir) {
   process.exit(1);
 }
 
-let yearlyFlag = null;
-let monthlyAvgFlag = null;
-let monthlyChartFlag = null;
 
- 
-// Loop through flags
+
 for (let i = 0; i < flags.length; i++) {
   switch (flags[i]) {
+    // Yearly report 
     case "-e": {
-      
-     
-      yearlyFlag = flags[i + 1];
-      let resultYearly = yearlyReport(dataDir, yearlyFlag);
-      console.log(
-        `\nHighest: ${resultYearly.highestTemp.value}C on ${formatDate(
-          resultYearly.highestTemp.date
-        )} \nLowest: ${resultYearly.lowestTemp.value}C on ${formatDate(
-          resultYearly.lowestTemp.date
-        )} \nHumidity: ${resultYearly.mostHumidDay.value}% on ${formatDate(
-          resultYearly.mostHumidDay.date
-        )} \n`
-      );
-
-      
-
       i++;
       break;
     }
 
+    // Monthly Average Report
     case "-a": {
-      
-      
-      monthlyAvgFlag = flags[i + 1];
-      let resultAverage = monthlyAverageReport(dataDir, monthlyAvgFlag);
-      console.log(resultAverage);
       i++;
       break;
     }
 
+    // Monthly Chart Report
     case "-c": {
-      monthlyChartFlag = flags[i + 1];
-      let resultChart:ChartReport[] = monthlyChartReport(dataDir, monthlyChartFlag);
-
-      if(flags.length==2){
-      resultChart.forEach((item) => {
-        let highBar = "+".repeat(item.highTemperature);
-        console.log(
-          `${item.lowTemperature} ${highBar} ${item.highTemperature}C`
-        );
-      });}
-
-      else{
-        if(flags.length>2)
-           console.log("date highTemp lowTemp humidity")
-          resultChart.forEach((item,index:number) => {
-        console.log(
-          `${monthlyChartFlag}/${index+1} ${item.highTemperature} ${item.lowTemperature} ${item.meanHumidity}`
-        );
-      })
-      }
+     
       i++;
       break;
     }
