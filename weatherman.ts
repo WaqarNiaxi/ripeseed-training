1837aeaef9810977a7a82501f64cf43776ac160e
waqar bin zafar <waqar.zafar@ripeseed.io>
2025-12-10 18:30:23 +0500

    fix: parse and collect the raw data in main file(optimize the code)

--- weatherman.ts
+++ weatherman.ts
@@ -7,50 +7,36 @@ const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const dataParser = require("./src/parser/weatherParser");
 const { getFileDataByMonth, yearlyData } = require("./src/helper/common");
-import type {
-  WeatherData
-} from "./src/interfaces/WeatherInterface.types";
+import type { WeatherData } from "./src/interfaces/WeatherInterface.types";
 
 const argv = yargs(hideBin(process.argv))
-  .option("extremes", { alias: ["e"]})
-  .option("chart", { alias: ["c"]})
-  .option("average", { alias: ["a"]})
+  .option("extremes", { alias: ["e"] })
+  .option("chart", { alias: ["c"] })
+  .option("average", { alias: ["a"] })
   .parse();
 
+let dirPath = argv._[0];
 
-let dirPath=argv._[0];
 
-//  Yearly Extremes (-e flag)
+function loadData(flagValue: string, isYearly: boolean): WeatherData[] {
+  return isYearly
+    ? yearlyData(dirPath, flagValue, dataParser)
+    : getFileDataByMonth(dirPath, flagValue, dataParser);
+}
+
+
+
 if (argv.extremes) {
-
-  const data:WeatherData[] = yearlyData(
-    dirPath,
-    argv.extremes,
-    dataParser
-  );
-
+  const data = loadData(argv.extremes, true);
   yearlyReport(data);
 }
 
-// Monthly Charts (-c flag)
 if (argv.chart) {
-
-  const data: Array<WeatherData> = getFileDataByMonth(
-    dirPath,
-    argv.chart,
-    dataParser
-  );
-  monthlyChartReport(data,argv.chart, Object.keys(argv).length);
+  const data = loadData(argv.chart, false);
+  monthlyChartReport(data, argv.chart, Object.keys(argv).length);
 }
 
-//  Monthly Averages (-a flag)
 if (argv.average) {
-
-  const data: Array<WeatherData> = getFileDataByMonth(
-    dirPath,
-    argv.average,
-    dataParser
-  );
-
+  const data = loadData(argv.average, false);
   monthlyAverageReport(data);
 }
